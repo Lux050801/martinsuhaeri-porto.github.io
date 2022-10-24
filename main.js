0b5604e2f7d0afd3f67bd1158e814f4f60bcece8
Lux050801 <martinsuhaeri1111@gmail.com>
2022-10-24 12:59:38 +0700

    update animation

---
+++ main.js
@@ -0,0 +1,14 @@
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(`.about_me`);
+sr.reveal(`.tools_animate`,{delay:950});
+sr.reveal(`.project_1`);
+sr.reveal(`.contact_text`);
+sr.reveal(`.contact_icon`,{delay:950});
